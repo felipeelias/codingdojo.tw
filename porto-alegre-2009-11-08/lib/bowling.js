223e60c31a4a9fd3a147c1f100d48a8a2dbea2d1
Felipe Elias Philipp <felipeelias@gmail.com>
2009-12-01 23:39:48 -0200

    OneStrike pass

--- porto-alegre-2009-11-08/lib/bowling.js
+++ porto-alegre-2009-11-08/lib/bowling.js
@@ -5,6 +5,10 @@ var Game = function() {
     return rolls[index] + rolls[index - 1] === 10;
   }
   
+  function isStrike (index) {
+    return rolls[index] === 10;
+  }
+  
   return {
     roll: function(pins) {
       rolls.push(pins);
@@ -13,7 +17,9 @@ var Game = function() {
     score: function() {
       var score = 0;
       for (var i = 0; i < rolls.length; i++) {
-        if (isSpare(i))
+        if (isStrike(i))
+          score += rolls[i] + rolls[i + 1] + rolls[i + 2];
+        else if (isSpare(i))
           score += rolls[i] + rolls[i + 1];
         else
           score += rolls[i];
